328bcbce95bb81830c9a9b94356b958ee80f2c98
Vipul Sachdeva <sachd.vipu@gmail.com>
2024-03-30 20:13:22 -0400

    API Call on server

--- server.js
+++ server.js
@@ -1,12 +1,57 @@
 var express = require('express');
-var app = express();
+const path = require("path")
+const cacheControl = require('express-cache-controller');
+const fetch = require('node-fetch');
+const axios = require('axios');
 
+const app = express();
+app.use(
+    "/css",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+  )
+  app.use(
+    "/js",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+  )
+  app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+
+  app.use(cacheControl({
+    noCache: true
+  }));
 app.set('view engine', 'ejs');
+
+
+let cache = {};
+const CACHE_DURATION = 120000; 
+
 
 app.get('/', function(req, res) {
 res.render('pages/home');
 });
 
-app.listen(3000);
+
+
+  app.get('/search', async (req, res) => {
+    try {
+        const { apt, dep, arr } = req.query;
+        let queryParams = { apt };
+        
+        if (dep) queryParams.dep = dep;
+        if (arr) queryParams.arr = arr;
+        
+        const response = await axios.get(`https://api.aviationapi.com/v1/vatsim/pilots`, {
+      params: queryParams
+    });
+    console.log(response.data);
+
+      res.render('pages/results', { data: response.data });
+    } catch (error) {
+      res.send(error.message);
+    }
+  });
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
 // Ref: https://mammothinteractive.com/build-a-hello-world-website-and-web-server-with-ejs-node-and-express/
